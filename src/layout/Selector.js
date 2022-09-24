0f37721e09dcc0d429be62a27f1517eae93e20c8
jCloud <jwlim102@gmail.com>
2022-09-24 23:09:24 +0900

    feat: 좌하단에 컬러 선택 UI 추가

--- src/layout/Selector.js
+++ src/layout/Selector.js
@@ -1,18 +1,67 @@
+import { useState } from 'react'
+import { iphone, iphonePro } from '../static/const.js'
+
 function Selector () {
+  const [model, setModel] = useState('iphone')
+
+  const renderModel = () => {
+    if (model === 'iphone') {
+      return (
+        <div className="flex gap-5 p-2 w-1/3">
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphone.blue.src } alt={ iphone.blue.label } />
+            <p className="text-center text-xs">Blue</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphone.purple.src } alt={ iphone.purple.label } />
+            <p className="text-center text-xs">Purple</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphone.midNight.src } alt={ iphone.midNight.label } />
+            <p className="text-center text-xs">Midnight</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphone.starLight.src } alt={ iphone.starLight.label } />
+            <p className="text-center text-xs">Starlight</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphone.red.src } alt={ iphone.red.label } />
+            <p className="text-center text-xs">Red</p>
+          </div>
+        </div>
+      )
+    }
+    if (model === 'iphonePro') {
+      return (
+        <div className="flex gap-5 p-2 w-1/3">
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphonePro.deepPurple.src } alt={ iphonePro.deepPurple.label } />
+            <p className="text-center text-xs">Deep Purple</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphonePro.gold.src } alt={ iphonePro.gold.label } />
+            <p className="text-center text-xs">Gold</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphonePro.silver.src } alt={ iphonePro.silver.label } />
+            <p className="text-center text-xs">Silver</p>
+          </div>
+          <div className="w-1/5 flex flex-col gap-2">
+            <img className="w-4/5 mx-auto" src={ iphonePro.spaceBlack.src } alt={ iphonePro.spaceBlack.label } />
+            <p className="text-center text-xs">Space Black</p>
+          </div>
+        </div>
+      )
+    }
+
+    return <></>
+  }
+
+
   return (
     <div className="absolute w-full left-0 bottom-0">
       <div className="p-5 flex justify-between">
-        <div className="flex gap-3">
-          <div>딥 퍼플</div>
-          <div>골드</div>
-          <div>실버</div>
-          <div>스페이스 그레이</div>
-        </div>
-        <div className="flex gap-3">
-          <div>iPhone 14</div>
-          <div>iPhone 14 Pro</div>
-          <div>iPhone 14 Pro Max</div>
-        </div>
+        { renderModel() }
       </div>
     </div>
   )
---
+++ src/static/const.js
@@ -0,0 +1,45 @@
+const iphone = {
+  red: {
+    src: require('../assets/iphone-14-red.png'),
+    label: 'iphone 14 red',
+  },
+  blue: {
+    src: require('../assets/iphone-14-blue.png'),
+    label: 'iphone 14 blue',
+  },
+  midNight: {
+    src: require('../assets/iphone-14-midnight.png'),
+    label: 'iphone 14 midnight',
+  },
+  purple: {
+    src: require('../assets/iphone-14-purple.png'),
+    label: 'iphone 14 purple',
+  },
+  starLight: {
+    src: require('../assets/iphone-14-starlight.png'),
+    label: 'iphone 14 starlight',
+  },
+}
+const iphonePro = {
+  deepPurple: {
+    src: require('../assets/iphone-14-pro-deeppurple.png'),
+    label: 'iphone 14 Pro deeppurple',
+  },
+  gold: {
+    src: require('../assets/iphone-14-pro-gold.png'),
+    label: 'iphone 14 Pro gold',
+  },
+  silver: {
+    src: require('../assets/iphone-14-pro-silver.png'),
+    label: 'iphone 14 Pro silver',
+  },
+  spaceBlack: {
+    src: require('../assets/iphone-14-pro-spaceblack.png'),
+    label: 'iphone 14 Pro spaceblack',
+  },
+}
+
+export {
+  iphone,
+  iphonePro,
+}
