e77bd27c30756cb381786db9646e97d8d68449c9
jCloud <jwlim102@gmail.com>
2022-09-24 23:14:49 +0900

    feat: 로고 이미지 추가

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,13 @@
+import logoSrc from '../assets/jCloud.svg'
+
 function Header () {
   return (
-    <div className="p-5 flex justify-between">
+    <div className="px-5 flex justify-between items-center">
       <div className="name">
         iPhone 14
       </div>
-      <div className="Logo">
-        Logo Img
+      <div className="Logo w-32">
+        <img src={ logoSrc } alt="logo" />
       </div>
     </div>
   )
